0714ce75491a4c0c58e5edeb5abe381413233a3a
Lukas <Bone008@users.noreply.github.com>
2018-12-07 13:43:56 +0100

    use Eigen in the sample program, closes #1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 
 #include <iostream>
+#include <Eigen/Dense>
 
 int main() {
-	std::cout << "Hello world!" << std::endl;
+	std::cout << "Hello Eigen!" << std::endl;
+
+	Eigen::Matrix2f m;
+	m << 1, 2, 3, 4;
+	std::cout << m << std::endl;
+
 	std::cin.get();
 	return 0;
 }
