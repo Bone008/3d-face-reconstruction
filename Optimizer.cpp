33416647354934293d07315c09ca3f45f5788fc5
Lukas <Bone008@users.noreply.github.com>
2018-12-20 20:54:46 +0100

    progress on Ceres solving: per-pixel residual computation (point to point distances)

--- Optimizer.cpp
+++ Optimizer.cpp
@@ -3,80 +3,144 @@
 
 using namespace Eigen;
 
+// Constant to allow better compile-time optimization.
+const unsigned int NUM_EIGEN_VEC = 160;
+
 struct ResidualFunctor {
-	// x is the source (average mesh), y is the target (input cloud)
-	ResidualFunctor(int px, int py, const PixelData& rasterizerResult, const VectorXf&& averageFaceVertices, const MatrixXf& shapeBasis, const Matrix4f& pose)
-		: px(px), py(py), rasterizerResult(rasterizerResult), averageFaceVertices(averageFaceVertices), shapeBasis(shapeBasis), pose(pose) {}
+	// x is the source (pos mesh), y is the target (input cloud)
+	ResidualFunctor(const pcl::PointXYZRGB& inputPoint, const PixelData& rasterizerResult, const VectorXf& averageFaceVertices, const MatrixXf& shapeBasis, const Matrix4f& pose)
+		: inputPoint(inputPoint), rasterizerResult(rasterizerResult), averageFaceVertices(averageFaceVertices), shapeBasis(shapeBasis), pose(pose) {}
 
 	template <typename T>
 	bool operator()(T const* alpha, T* residual) const {
-		rasterizerResult.vertexIndices;
+		if (!rasterizerResult.isValid) {
+			// Skip pixels where Steve isn't rendered into.
+			residual[0] = T(0);
+			residual[1] = T(0);
+			residual[2] = T(0);
+			return true;
+		}
 
-		// TODO do the things
+		std::cout << "    Evaluating residual " << &inputPoint << " ...";
 
-		//T baseTimesAlpha = T(0);
-		//for (int e = 0; e < nEigenVec_; e++) {
-		//	baseTimesAlpha += T(base_(i_, e)) * alpha[e];
-		//}
+		T worldSpacePixel[] = { T(0), T(0), T(0) };
 
-		//residual[0] = T(y_) - (T(x_) + baseTimesAlpha);
+		// For each vertex that is part of the triangle at this pixel.
+		for (int i = 0; i < 3; i++) {
+			int vertexIndex = rasterizerResult.vertexIndices[i];
+			double barycentricFactor = rasterizerResult.barycentricCoordinates[i];
+
+			// Vertex position of average face.
+			T pos[] = {
+				T(averageFaceVertices(3 * vertexIndex + 0)),
+				T(averageFaceVertices(3 * vertexIndex + 1)),
+				T(averageFaceVertices(3 * vertexIndex + 2)),
+			};
+
+			// Displace by applying alpha.
+			for (int j = 0; j < shapeBasis.cols(); j++) {
+				pos[0] += T(shapeBasis(3 * vertexIndex + 0, j)) * alpha[j];
+				pos[1] += T(shapeBasis(3 * vertexIndex + 1, j)) * alpha[j];
+				pos[2] += T(shapeBasis(3 * vertexIndex + 2, j)) * alpha[j];
+			}
+
+			// Transform to world space.
+			T worldPos[] = {
+				T(pose(0,0)) * pos[0] + T(pose(0,1)) * pos[1] + T(pose(0,2)) * pos[2] + T(pose(0,3)),
+				T(pose(1,0)) * pos[0] + T(pose(1,1)) * pos[1] + T(pose(1,2)) * pos[2] + T(pose(1,3)),
+				T(pose(2,0)) * pos[0] + T(pose(2,1)) * pos[1] + T(pose(2,2)) * pos[2] + T(pose(2,3)),
+			};
+
+			worldSpacePixel[0] += barycentricFactor * worldPos[0];
+			worldSpacePixel[1] += barycentricFactor * worldPos[1];
+			worldSpacePixel[2] += barycentricFactor * worldPos[2];
+		}
+
+		residual[0] = T(inputPoint.x) - worldSpacePixel[0];
+		residual[1] = T(inputPoint.y) - worldSpacePixel[1];
+		residual[2] = T(inputPoint.z) - worldSpacePixel[2];
+
+		std::cout << " Done!" << std::endl;
+
+		// TODO: point to plane distance, but for this we need normals
+		// TODO: compare colors once we process albedo
 		return true;
 	}
 
 private:
-	// Pixel positions of this residual.
-	const int px, py;
+	// Input pixel that this residual is computing.
+	const pcl::PointXYZRGB& inputPoint;
 
+	// Shape (3 * numVertices,)
 	const VectorXf& averageFaceVertices;
+	// Shape (3 * numVertices, numEigenVec)
 	const MatrixXf& shapeBasis;
 	const Matrix4f& pose;
 
 	// Rasterization result for this pixel.
 	const PixelData& rasterizerResult;
-
-	// TODO missing: camera intrinsics, pointer to rasterization result, pointer to input sensor depth map
 };
 
-FaceParameters optimizeParameters(FaceModel& model, const Eigen::Matrix4f& pose, const Sensor& inputSensor) {
-	/* high-level overview of what should probably happen:
+struct MyIterationCallback : public ceres::IterationCallback {
+	virtual ceres::CallbackReturnType operator()(const ceres::IterationSummary& summary) override {
+		std::cout << "iteration callback " << summary.cost << std::endl;
+		return ceres::CallbackReturnType::SOLVER_CONTINUE;
+	}
+};
 
+FaceParameters optimizeParameters(FaceModel& model, const Matrix4f& pose, const Sensor& inputSensor) {
 
-	alpha = 0
-	for each pixel (i,j) in depth_map:
-		init_residual(inputSensor.depth_map, i, j, model.shapeBasis, model.averageFace, pose, inputSensor.camera)
+	const uint32_t width = inputSensor.m_cloud->width;
+	const uint32_t height = inputSensor.m_cloud->height;
+	std::array<double, NUM_EIGEN_VEC> alpha{};
 
-	for each ceres iteration:
-		barycentric_buffer, vertex_index_buffer = rasterize_steve(inputSensor.intrinsics, model, pose)
-		r.compute(barycentric_buffer, vertex_index_buffer)
-	*/
+	std::vector<PixelData> rasterResults(width * height);
+
+	// Add some dummy rasterization results.
+	rasterResults[1 * width + 193] = { {1, 3, 5}, {0.1, 0.3, 0.6}, true };
+	rasterResults[1 * width + 225] = { {25, 3246, 1231}, {0.2, 0.4, 0.4}, true };
+	rasterResults[1 * width + 274] = { {62, 22, 44}, {0.45, 0.15, 0.4}, true };
 
 	ceres::Problem problem;
+	for (unsigned int y = 0; y < height; y++) {
+		for (unsigned int x = 0; x < width; x++) {
+			const pcl::PointXYZRGB& point = (*inputSensor.m_cloud)(x, y);
+			if (std::isnan(point.z)) {
+				continue;
+			}
 
-	unsigned int numPixels = 5;
+			// Temporary until we no longer use dummy data for the rasterizer.
+			if (!rasterResults[y*width + x].isValid) continue;
 
-	double* alpha = new double[model.getNumEigenVec()];
-	for (unsigned int i = 0; i < numPixels; i++) {
-		//ceres::CostFunction *cost_function =
-		//	new ceres::AutoDiffCostFunction<ResidualFunctor, 1, 2>(
-		//		new ResidualFunctor(/* TODO fill this */));
-		//problem.AddResidualBlock(cost_function, NULL, alpha);
+			ceres::CostFunction* costFunc = new ceres::AutoDiffCostFunction<ResidualFunctor, 3, NUM_EIGEN_VEC>(
+					new ResidualFunctor(point, rasterResults[y * width + x], model.m_averageShapeMesh.vertices, model.m_shapeBasis, pose));
+			problem.AddResidualBlock(costFunc, NULL, alpha.data());
+		}
 	}
+	std::cout << "Cost function has " << problem.NumResiduals() << " scalar residuals." << std::endl;
+
+	MyIterationCallback callback;
 
 	ceres::Solver::Options options;
-	options.linear_solver_type = ceres::DENSE_QR;
 	options.minimizer_progress_to_stdout = true;
+	options.update_state_every_iteration = true;
+	options.callbacks.push_back(&callback);
 	ceres::Solver::Summary summary;
 	ceres::Solve(options, &problem, &summary);
 
-	std::cout << summary.BriefReport() << std::endl;
+	std::cout << summary.FullReport() << std::endl;
+	std::cout << "Some final values of alpha: " << Map<VectorXd>(alpha.data(), 10) << std::endl;
 
 	FaceParameters params;
-	// for now, return 0
-	params.alpha.setZero(model.getNumEigenVec());
+	params.alpha = Map<VectorXd>(alpha.data(), NUM_EIGEN_VEC).cast<float>();
+	
+	// For now, don't actually return the values because they don't make sense yet.
+	params.alpha.setZero();
 	return params;
 }
 
-std::vector<PixelData> rasterizeVertices(const double* vertices, const Eigen::MatrixX3i& triangleList, int width, int height)
+// vertices are already projected
+std::vector<PixelData> rasterizeVertices(const MatrixXf* vertices, const MatrixX3i& triangleList, int width, int height)
 {
 	return std::vector<PixelData>();
 }
