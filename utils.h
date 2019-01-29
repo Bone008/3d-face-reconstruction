53842674483f61142c866df2608afd6541789742
Lukas <Bone008@users.noreply.github.com>
2019-01-29 17:18:16 +0100

    put BMP saving into a utility + fix depth image color calculation

--- utils.h
+++ utils.h
@@ -1,10 +1,9 @@
 #pragma once
 
-#include <pcl/common/common.h>
-#include <pcl/Vertices.h>
-
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointsToCloud(const Eigen::VectorXf& points);
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointsToCloud(const Eigen::VectorXf& points, const Eigen::Matrix4Xi& vertexColors);
 pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr pointsToCloud(const Eigen::VectorXf& points, const Eigen::Matrix3Xf& normals);
 
 std::vector<pcl::Vertices> trianglesToVertexList(const Eigen::Matrix3Xi& triangles);
+
+void saveBitmap(const char* filename, int width, int height, std::function<Eigen::Vector4i(unsigned int x, unsigned int y)> pixelColorProvider);
